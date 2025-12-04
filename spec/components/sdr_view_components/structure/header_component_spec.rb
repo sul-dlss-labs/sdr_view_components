4207b93898b7463c3df114c23446cba37bf27c61
Aaron Collier <aaron.collier@stanford.edu>
2025-12-04 14:07:07 -0800

    Allow for selection of the sul_logo in header. (#13)

--- spec/components/sdr_view_components/structure/header_component_spec.rb
+++ spec/components/sdr_view_components/structure/header_component_spec.rb
@@ -3,11 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe SdrViewComponents::Structure::HeaderComponent, type: :component do
-  it 'renders title and subtitle' do
+  it 'renders title and subtitle' do # rubocop:disable RSpec/ExampleLength
     render_inline(described_class.new(title: 'Test Header', subtitle: 'Test Subtitle'))
 
     expect(page).to have_text('Test Header')
     expect(page).to have_text('Test Subtitle')
+    within('.container') do
+      expect(page).to have_link('Stanford University Libraries', class: 'navbar-logo')
+    end
+  end
+
+  context 'when sul_logo is polychrome' do
+    it 'renders sul logo with polychrome class' do
+      render_inline(described_class.new(title: 'Test Header', sul_logo: 'polychrome'))
+      within('.container') do
+        expect(page).to have_link('Stanford University Libraries', class: 'navbar-logo polychrome')
+      end
+    end
   end
 
   context 'when subtitle is not provided' do
